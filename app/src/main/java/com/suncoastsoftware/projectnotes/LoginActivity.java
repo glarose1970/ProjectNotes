52c24353939bddf8680af9729faf34a5038b5967
Mike <inkedgfx@tampabay.rr.com>
2017-10-22 11:52:33 -0400

    add firebase to project

--- app/src/main/java/com/suncoastsoftware/projectnotes/LoginActivity.java
+++ app/src/main/java/com/suncoastsoftware/projectnotes/LoginActivity.java
@@ -1,13 +1,36 @@
 package com.suncoastsoftware.projectnotes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //check to see if the user is logged in
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    //user is signed in
+                }else {
+                    //code to log in user here
+                }
+            }
+        };
     }
 }
